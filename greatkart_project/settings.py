b69ad1dc393b05eb5cfdf4498e0c900688dbf159
Ose <ose.sunday@gmail.com>
2023-08-17 22:10:22 +0100

    vcl

--- greatkart_project/settings.py
+++ greatkart_project/settings.py
@@ -112,8 +112,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build' 'static')
-
-
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 
 # media files configuration
